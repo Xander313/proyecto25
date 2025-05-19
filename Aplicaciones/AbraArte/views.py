2bcd36cad4a4bdedda1736cc921b22f07c8d50e5
Eddylucero <luceroeddy8@gmail.com>
2025-05-19 17:04:06 -0500

    Programacion apartado Obra de Arte

--- Aplicaciones/AbraArte/views.py
+++ Aplicaciones/AbraArte/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import ObraArte
+from django.contrib import messages
 
 # Create your views here.
+def inicioArte(request):
+    listadoArte = ObraArte.objects.all()
+    return render(request, 'inicioArte.html', {'listadoArte': listadoArte})
--- proyecto25/urls.py
+++ proyecto25/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AbraArte/', include('Aplicaciones.AbraArte.urls')),
+    path('ObraArte/', include('Aplicaciones.AbraArte.urls')),
     path('Exposicion/', include('Aplicaciones.Exposicion.urls')),
     path('Reserva/', include('Aplicaciones.Reserva.urls')),
     path('Visitante/', include('Aplicaciones.Visitante.urls')),
