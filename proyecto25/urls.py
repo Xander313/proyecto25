1d9e88ff58dbc38b81403c9a1b00c7d057dd5eec
Eddylucero <luceroeddy8@gmail.com>
2025-05-19 16:52:29 -0500

    Edicion de la urls.py principal

--- proyecto25/urls.py
+++ proyecto25/urls.py
@@ -1,22 +1,11 @@
-"""
-URL configuration for proyecto25 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""proyecto25 URL Configuration"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('AbraArte/', include('Aplicaciones.AbraArte.urls')),
+    path('Exposicion/', include('Aplicaciones.Exposicion.urls')),
+    path('Reserva/', include('Aplicaciones.Reserva.urls')),
+    path('Visitante/', include('Aplicaciones.Visitante.urls')),
 ]
