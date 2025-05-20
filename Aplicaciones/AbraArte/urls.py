e24fcb195fdee5a39ca3c7eaaf8cb2b674a46adb
Eddylucero <luceroeddy8@gmail.com>
2025-05-19 20:22:44 -0500

    Creacion de gitinore y programacion de aplicacion Obra de Arte

--- Aplicaciones/AbraArte/urls.py
+++ Aplicaciones/AbraArte/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicioArte),
+    path('', views.inicioArte),  
+    path('nuevoArte', views.nuevaArte),  
+    path('guardarArte', views.guardarArte),  
+    path('eliminarArte/<id>', views.eliminarArte),  
+    path('editarArte/<id>', views.editarArte),  
+    path('procesarEdicionArte', views.procesarEdicionArte), 
 ]
--- Aplicaciones/AbraArte/views.py
+++ Aplicaciones/AbraArte/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ObraArte
-from django.contrib import messages
 
-# Create your views here.
+# Renderizar listado de obras de arte
 def inicioArte(request):
-    listadoArte = ObraArte.objects.all()
-    return render(request, 'inicioArte.html', {'listadoArte': listadoArte})
+    listadoObras = ObraArte.objects.all()
+    return render(request, "ObraArte/inicioArte.html", {'obras': listadoObras})
+
+# Renderizar formulario para agregar una nueva obra de arte
+def nuevaArte(request):
+    return render(request, "ObraArte/nuevoArte.html")
+
+# Almacenar datos de la obra en la BD
+def guardarArte(request):
+    if request.method == "POST":
+        titulo = request.POST["titulo"]
+        autor = request.POST["autor"]
+        anio = request.POST["anio"]
+
+        ObraArte.objects.create(titulo=titulo, autor=autor, anio=anio)
+        return redirect('/inicioArte/')
+    return redirect('/nuevoArte/')  
+
+# Eliminar obra de arte por ID
+def eliminarArte(request, id):
+    obraEliminar = ObraArte.objects.get(id=id)
+    obraEliminar.delete()
+    return redirect('/inicioArte/')
+
+# Renderizar formulario de edición de obra de arte
+def editarArte(request, id):
+    obra = ObraArte.objects.get(id=id)
+    return render(request, "ObraArte/editarArte.html", {'obra': obra})
+
+# Procesar la edición y actualizar en la BD
+def procesarEdicionArte(request):
+    id = request.POST["id"]
+    titulo = request.POST["titulo"]
+    autor = request.POST["autor"]
+    anio = request.POST["anio"]
+
+    obra = ObraArte.objects.get(id=id)
+    obra.titulo = titulo
+    obra.autor = autor
+    obra.anio = anio
+    obra.save()
+    
+    return redirect('/inicioArte/')
