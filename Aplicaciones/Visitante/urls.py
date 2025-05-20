62e32aa6e37443e04b3cfc2d4d187fdf87099480
Eddylucero <luceroeddy8@gmail.com>
2025-05-19 22:24:12 -0500

    Crud completo de Aplicaciones de Visitante

--- Aplicaciones/Visitante/urls.py
+++ Aplicaciones/Visitante/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('', views.inicioVisitante, name='indexVisitante'),  
+    path('nuevoVisitante', views.nuevoVisitante, name='nuevoVisitante'),  
+    path('guardarVisitante', views.guardarVisitante, name='guardarVisitante'),  
+    path('eliminarVisitante/<id>', views.eliminarVisitante),  
+    path('editarVisitante/<id>', views.editarVisitante, name='editarVisitante'),  
+    path('procesarEdicionVisitante/<id>', views.procesarEdicionVisitante, name='procesarEdicionVisitante'),  
 ]
--- Aplicaciones/Visitante/views.py
+++ Aplicaciones/Visitante/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Visitante
+from django.contrib import messages
 
-# Create your views here.
+# Renderizar listado de visitantes
+def inicioVisitante(request):
+    listadoVisitantes = Visitante.objects.all()
+    return render(request, "Visitante/inicioVisitante.html", {'visitantes': listadoVisitantes})
+
+# Renderizar formulario para agregar un nuevo visitante
+def nuevoVisitante(request):
+    return render(request, "Visitante/nuevoVisitante.html")
+
+# Almacenar datos del visitante en la BD
+def guardarVisitante(request):
+    if request.method == "POST":
+        nombre = request.POST["nombre"]
+        email = request.POST["email"]
+
+        Visitante.objects.create(nombre=nombre, email=email)
+        messages.success(request, "SE HA CREADO EL VISITANTE")
+
+        return redirect('indexVisitante')
+    return redirect('indexVisitante')
+
+# Eliminar visitante por ID
+def eliminarVisitante(request, id):
+    visitanteEliminar = Visitante.objects.get(id=id)
+    visitanteEliminar.delete()
+    messages.success(request, "SE HA ELIMINADO EL VISITANTE")
+    return redirect('indexVisitante')
+
+# Renderizar formulario de edición de visitante
+def editarVisitante(request, id):
+    visitante = Visitante.objects.get(id=id)
+    return render(request, "Visitante/editarVisitante.html", {'visitante': visitante})
+
+# Procesar la edición y actualizar en la BD
+def procesarEdicionVisitante(request, id):
+    nombre = request.POST["nombre"]
+    email = request.POST["email"]
+
+    visitante = Visitante.objects.get(id=id)
+    visitante.nombre = nombre
+    visitante.email = email
+    visitante.save()
+    messages.success(request, "SE HA EDITADO EL VISITANTE")
+
+    return redirect('indexVisitante')
