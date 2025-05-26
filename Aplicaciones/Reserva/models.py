14113af8dfd4797e35a3c1f5e7f68def127d8afb
Xander313 <jhonn.roldan8626@utc.edu.ec>
2025-05-26 13:20:53 -0500

    Se ha modificado el formulario de Reserva

--- Aplicaciones/Reserva/models.py
+++ Aplicaciones/Reserva/models.py
@@ -7,6 +7,7 @@ class Reserva(models.Model):
     visitante = models.ForeignKey(Visitante, on_delete=models.CASCADE)  
     exposicion = models.ForeignKey(Exposicion, on_delete=models.CASCADE)  
     fecha_reserva = models.DateTimeField(auto_now_add=True)
+    observaciones = models.TextField(default="")
 
     def _str_(self):
         return f"{self.visitante} - {self.exposicion}"
