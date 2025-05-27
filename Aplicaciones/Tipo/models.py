09814313091863c6a79ef38660e2601e13599106
Eddylucero <luceroeddy8@gmail.com>
2025-05-26 20:04:03 -0500

    Creacion de modelos de Tipo

--- Aplicaciones/Tipo/models.py
+++ Aplicaciones/Tipo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Tipo(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=255)
+    anio = models.CharField(max_length=20)
+    genero = models.CharField(max_length=20)
