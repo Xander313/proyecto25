79fe94b9b8c7bab7ebb5dbc4e435eb1d31283060
Eddylucero <luceroeddy8@gmail.com>
2025-05-26 20:07:03 -0500

    Creacion del model de Artista

--- Aplicaciones/Artista/models.py
+++ Aplicaciones/Artista/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Artista(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    pais = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=10)
+    edad = models.CharField(max_length=5)
+    genero = models.CharField(max_length=10)
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)  # Relación directa
